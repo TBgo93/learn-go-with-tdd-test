89f15d1dc6bb9231c2bf901b32dfd7ba680e6f68
TBgo93 <tbaigorria93@gmail.com>
2023-02-05 19:12:28 -0300

    Structs, Interface and SuiteTests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,3 +1,82 @@
 package structs
 
 import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+
+		checkArea(t, rectangle, want)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
+}
+
+func TestAreaRefactor(t *testing.T) {
+	type structTest struct {
+		shape Shape
+		want  float64
+	}
+	areaTests := []structTest{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		want := tt.want
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+}
+
+func TestAreaRefactoTwo(t *testing.T) {
+	type structTest struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}
+
+	areaTests := []structTest{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
+
+}
