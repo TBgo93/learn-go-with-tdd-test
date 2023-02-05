04b4e1641933f3bd2892e82d903f95d1e2d0029b
TBgo93 <tbaigorria93@gmail.com>
2023-02-05 20:56:26 -0300

    Pointers

---
+++ pointers/wallet_test.go
@@ -0,0 +1,49 @@
+package pointers
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+	assertError := func(t testing.TB, got, want error) {
+		if got == nil {
+			t.Fatal("wanted an error but didnt get one")
+		}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(10)
+
+		if err != nil {
+			assertError(t, err, ErrInsufficientFunds)
+		}
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
+	})
+}
