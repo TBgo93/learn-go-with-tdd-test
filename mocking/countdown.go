c814c5bf0206ea01273bb1d838d555f203485fbf
TBgo93 <tbaigorria93@gmail.com>
2023-02-13 22:22:36 -0300

    Mocking

---
+++ mocking/countdown.go
@@ -0,0 +1,39 @@
+package countdown
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; 0 < i; i-- {
+		sleeper.Sleep()
+	}
+	for i := countdownStart; 0 < i; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
